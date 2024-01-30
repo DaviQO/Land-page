baaae451e7f7045cf0012b33cb02acfd2fb042c7
Davi Sousa <132115852+DaviQO@users.noreply.github.com>
2024-01-30 19:38:18 -0300

    Land page HTML CSS SCSS JS
    Land page HTML CSS SCSS JS com modelo

---
+++ MINI WEBSITE UTILAZANDO SCSS JS/src/js/website.js
@@ -0,0 +1,6 @@
+//navbar
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
